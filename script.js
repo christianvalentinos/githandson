ef53e9cfb82639289d8320da9a646e3829d56d45
Leon <leonelshenaandreas16@gmail.com>
2026-09-19 13:36:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const members = [
     skills: ["HTML", "CSS", "JavaScript"]
   },
   {
-    name: "Nama Anggota 2",
+    name: "Leonel Shena Andreas",
     role: "UI/UX Designer",
     image: "https://i.pravatar.cc/300?img=47",
     description: "Saya merancang pengalaman pengguna yang sederhana, menarik, dan mudah dipahami dengan memperhatikan kebutuhan pengguna di setiap halaman.",
